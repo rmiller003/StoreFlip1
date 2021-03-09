3bf7087e2eda9981c219bedde4690a46a954ff17
Robert Miller <robert.miller@loblaw.ca>
2021-03-09 15:23:47 -0500

    Stage 1

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("http://rmiller003.pythonanywhere.com")
+driver.get("https://10.104.8.111:8443/racommon/NSLogin.jsp?redirect=%2Fconsole%2F")
 print(driver.title)
+
+search = driver.find_element_by_id('details-button')
+search.send_keys(Keys.RETURN)
+search = driver.find_element_by_id('proceed-link')
+search.send_keys(Keys.RETURN)
+
+time.sleep(10)
+
+#driver.quit()
